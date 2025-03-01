e7c5674c7464bff8c02623ba2e89d2323e60c608
Balaji-techi <mdubalaji44@gmail.com>
2025-03-01 16:23:11 +0530

    Add files via upload

---
+++ AssetManagement/Vendor.java
@@ -0,0 +1,18 @@
+package Practice.AssetManagement;
+public class Vendor {
+    private String vendorId;
+    private String name;
+
+    public Vendor(String vendorId, String name) {
+        this.vendorId = vendorId;
+        this.name = name;
+    }
+
+    public String getVendorId() {
+        return vendorId;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
